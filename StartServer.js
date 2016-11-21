d71ddd28e27a040524f61c212ce5c5d026ab3d47
kks1181 <rlarbtjr1181@naver.com>
2016-11-21 13:55:43 +0900

    rendering html file includeing .css and . js

--- StartServer.js
+++ StartServer.js
@@ -1,15 +1,48 @@
 var fs = require('fs');
 var express = require('express');
 var multiparty = require('multiparty');
+var path = require('path');
 var app = express();
 
+app.use(express.static('images'));
+app.use(express.static('assets/css'));
+app.use(express.static('assets/js'));
+
 app.get('/',function(req, res){
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+	res.write('<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>');
+	res.write('<script src="https://ajax.googleapis.com/ajax/libs/threejs/r76/three.min.js"></script>');
+	res.write('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>');
+	res.write('<link href="https://opensource.keycdn.com/fontawesome/4.7.0/font-awesome.min.css" />');
+
+	fs.readFile('./assets/js/main.js', function (error, data) {
+		res.write('<script>' + data + '</script>')});
+	fs.readFile('./assets/css/main.css', function (error, data) {
+		res.write('<style>' + data + '</style>')});
+	fs.readFile('./assets/js/jquery.poptrox.min.js', function (error, data) {
+		res.write('<script>' + data + '</script>')});
+	fs.readFile('./assets/js/jquery.scrolly.min.js', function (error, data) {
+		res.write('<script>' + data + '</script>')});
+	fs.readFile('./assets/js/jquery.scrollex.min.js', function (error, data) {
+		res.write('<script>' + data + '</script>')});
+	fs.readFile('./assets/js/skel.min.js', function (error, data) {
+		res.write('<script>' + data + '</script>')});
+	fs.readFile('./assets/js/util.js', function (error, data) {
+		res.write('<script>' + data + '</script>')});
 	fs.readFile('index.html', function (error, data) {
+		res.write(data, function (error) {
+			console.log(error);
+    });
+  });
+	res.end();
+});
+
+app.get('/plyControl.html',function(req, res){
+	fs.readFile('plyControl.html', function (error, data) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(data, function (error) {
             console.log(error);
         });
-		//res.send("<script> alert('hi'); </script>");
     });
 });
 
