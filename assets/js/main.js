d07b4307e5efc22e2bace0c01d24574fdc47b632
minuk8932 <minuk8932@naver.com>
2016-12-15 18:22:36 +0900

    google login
    api

--- assets/js/main.js
+++ assets/js/main.js
@@ -379,6 +379,70 @@ function autoType(elementClass, typingSpeed) {
                 }
             });
         });
+
+        //get apis
+
+  //       function logout()
+  // {
+  //     gapi.auth.signOut();
+  //     location.reload();
+  // }
+  // function login()
+  // {
+  //   var myParams = {
+  //     'clientid' : '787805438494-0u31hhqkpup1sq6h7s8atdqihvin2s6v.apps.googleusercontent.com',
+  //     'cookiepolicy' : 'single_host_origin',
+  //     'callback' : 'loginCallback',
+  //     'approvalprompt':'force',
+  //     'scope' : 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.profile.emails.read'
+  //   };
+  //   gapi.auth.signIn(myParams);
+  // }
+  //
+  // function loginCallback(result)
+  // {
+  //     if(result['status']['signed_in'])
+  //     {
+  //         var request = gapi.client.plus.people.get(
+  //         {
+  //             'userId': 'me'
+  //         });
+  //         request.execute(function (resp)
+  //         {
+  //             var email = '';
+  //             if(resp['emails'])
+  //             {
+  //                 for(i = 0; i < resp['emails'].length; i++)
+  //                 {
+  //                     if(resp['emails'][i]['type'] == 'account')
+  //                     {
+  //                         email = resp['emails'][i]['value'];
+  //                     }
+  //                 }
+  //             }
+  //
+  //             var str = "Name:" + resp['displayName'] + "<br>";
+  //             str += "Image:" + resp['image']['url'] + "<br>";
+  //             str += "<img src='" + resp['image']['url'] + "' /><br>";
+  //
+  //             str += "URL:" + resp['url'] + "<br>";
+  //             str += "Email:" + email + "<br>";
+  //             document.getElementById("profile").innerHTML = str;
+  //         });
+  //
+  //     }
+  //
+  // }
+  // function onLoadCallback()
+  // {
+  //     gapi.client.setApiKey('787805438494-0u31hhqkpup1sq6h7s8atdqihvin2s6v.apps.googleusercontent.com');
+  //     gapi.client.load('plus', 'v1',function(){});
+  // }
+
+
+
+
+
         // making login form
 
 
@@ -401,11 +465,67 @@ function autoType(elementClass, typingSpeed) {
             // Calling Register Form
             $("#ggBtn").click(function() {
                 function onSignIn(googleUser) {
-                    var profile = googleUser.getBasicProfile();
-                    console.log('ID: ' + profile.getId());
-                    console.log('Name: ' + profile.getName());
-                    console.log('Image URL: ' + profile.getImageUrl());
-                    console.log('Email: ' + profile.getEmail());
+                    // var profile = googleUser.getBasicProfile();
+                    // console.log('ID: ' + profile.getId());
+                    // console.log('Name: ' + profile.getName());
+                    // console.log('Image URL: ' + profile.getImageUrl());
+                    // console.log('Email: ' + profile.getEmail());
+                    function logout()
+  {
+      gapi.auth.signOut();
+      location.reload();
+  }
+  function login()
+  {
+    var myParams = {
+      'clientid' : '787805438494-0u31hhqkpup1sq6h7s8atdqihvin2s6v.apps.googleusercontent.com',
+      'cookiepolicy' : 'single_host_origin',
+      'callback' : 'loginCallback',
+      'approvalprompt':'force',
+      'scope' : 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.profile.emails.read'
+    };
+    gapi.auth.signIn(myParams);
+  }
+
+  function loginCallback(result)
+  {
+      if(result['status']['signed_in'])
+      {
+          var request = gapi.client.plus.people.get(
+          {
+              'userId': 'me'
+          });
+          request.execute(function (resp)
+          {
+              var email = '';
+              if(resp['emails'])
+              {
+                  for(i = 0; i < resp['emails'].length; i++)
+                  {
+                      if(resp['emails'][i]['type'] == 'account')
+                      {
+                          email = resp['emails'][i]['value'];
+                      }
+                  }
+              }
+
+              var str = "Name:" + resp['displayName'] + "<br>";
+              str += "Image:" + resp['image']['url'] + "<br>";
+              str += "<img src='" + resp['image']['url'] + "' /><br>";
+
+              str += "URL:" + resp['url'] + "<br>";
+              str += "Email:" + email + "<br>";
+              document.getElementById("profile").innerHTML = str;
+          });
+
+      }
+
+  }
+  function onLoadCallback()
+  {
+      gapi.client.setApiKey('787805438494-0u31hhqkpup1sq6h7s8atdqihvin2s6v.apps.googleusercontent.com');
+      gapi.client.load('plus', 'v1',function(){});
+  }
                 }
                 console.log("google+ login");
                 return false;
